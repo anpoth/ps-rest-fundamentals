b8d99941c5cae38f3e3e4c188604324dd0f96e1f
Alexander Poth <60898625+anpoth@users.noreply.github.com>
2026-03-17 19:28:32 +0000

    lorum ipsum

--- server/src/middleware/auth0.middleware.ts
+++ server/src/middleware/auth0.middleware.ts
@@ -5,5 +5,5 @@ dotenv.config();
 
 export const validateAccessToken = auth({
     issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
-    audience: process.env.AUTH0_AUDIENCE
+    audience: process.env.AUTH0_AUDIENCE,
 });
