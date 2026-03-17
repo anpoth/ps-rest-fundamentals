72fedb6c1361b756c57ee01a471bb18e36709c7b
Alexander Poth <60898625+anpoth@users.noreply.github.com>
2026-03-17 19:25:55 +0000

    fixed quotations

--- server/src/middleware/auth0.middleware.ts
+++ server/src/middleware/auth0.middleware.ts
@@ -4,6 +4,6 @@ import { auth } from "express-oauth2-jwt-bearer";
 dotenv.config();
 
 export const validateAccessToken = auth({
-    issuerBaseURL: 'https://${process.env.AUTH0_DOMAIN}',
+    issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
     audience: process.env.AUTH0_AUDIENCE
 });
